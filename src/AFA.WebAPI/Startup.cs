6226a16e231c26a4c9e9ec6c1907cb7fcb844f87
dirceu junior <dirceu.sjr@gmail.com>
2021-12-28 21:25:44 -0300

    injeção do AFA.Data na WebAPI

--- src/AFA.WebAPI/Startup.cs
+++ src/AFA.WebAPI/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using IStartup = AFA.WebAPI.Interfaces.IStartup;
+using AFA.Data;
+using AFA.Data.Extensions;
 
 namespace AFA.WebAPI;
 
@@ -22,6 +24,8 @@ public class Startup : IStartup
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+
+        services.AddDataContext<AFAContext>();
     }
 
     // Configure the HTTP request pipeline.
