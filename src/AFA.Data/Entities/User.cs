f1efd69756d7b4b5f37ffa2ec4bf7bcaf5d68add
dirceu junior <dirceu.sjr@gmail.com>
2022-01-03 22:06:18 -0300

    Adição de datas de eventos na entidade usuário

--- src/AFA.Data/Entities/User.cs
+++ src/AFA.Data/Entities/User.cs
@@ -7,7 +7,10 @@ public class User
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public bool EmailConfirmed { get; set; }
-    public bool Subscribed { get; set; }
+    public bool EmailConfirmed { get; set; } = false;
+    public bool Subscribed { get; set; } = false;
     public string SecurityStamp { get; set; }
+    public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+    public DateTime? EmailConfirmedAt { get; set; }
+    public DateTime? UnsubscribedAt { get; set; }  
 }
