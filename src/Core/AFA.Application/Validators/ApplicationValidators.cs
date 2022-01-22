9f77a9cc49f8d98b7e3a3987ac48227db6258611
dirceu junior <dirceu.sjr@gmail.com>
2022-01-22 19:00:56 -0300

    remoção de classe não utilizada

--- src/Core/AFA.Application/Validators/ApplicationValidators.cs
+++
@@ -1,9 +0,0 @@
-namespace AFA.Application.Validators;
-
-/// <summary>
-/// Classe para marcação do assembly
-/// </summary>
-public sealed class ApplicationValidators
-{
-
-}
--- src/Core/AFA.Application/Validators/IValidable.cs
+++ src/Core/AFA.Application/Validators/IValidable.cs
@@ -1,5 +1,9 @@
 namespace AFA.Application.Validators;
 
+/// <summary>
+/// Interface para marcação de objetos que podem ser validados.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public interface IValidable<T> where T : new()
 {
 
