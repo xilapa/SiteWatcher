89e084f08b1798b59e3574c70bb9c2e495d9546c
dirceu junior <dirceu.sjr@gmail.com>
2022-01-22 20:18:28 -0300

    descrição adicionada ao ESubscriptionResult

--- src/Core/AFA.Domain/Enums/ESubscriptionResult.cs
+++ src/Core/AFA.Domain/Enums/ESubscriptionResult.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel;
+
 namespace AFA.Domain.Enums;
 
 public enum ESubscriptionResult
 {
+    [Description("User subscribed successfully")]
     SubscribedSuccessfully = 1,
+
+    [Description("User already subscribed")]
     AlreadySubscribed = 2,
+
+    [Description("User unsubscribed successfully")]
     Unsubscribed = 3,
+
+    [Description("Email not confirmed")]
     EmailNotConfirmed = 4,
-    UserDoNotExist = 5
+
+    [Description("User does not exist")]
+    UserDoesNotExist = 5
 }
