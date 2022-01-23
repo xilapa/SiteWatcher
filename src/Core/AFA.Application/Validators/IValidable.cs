b49b5fff0412ffdadf3dd300b48a61790794cdc0
dirceu junior <dirceu.sjr@gmail.com>
2022-01-22 22:56:17 -0300

    ajuste de comentário

--- src/Core/AFA.Application/Validators/IValidable.cs
+++ src/Core/AFA.Application/Validators/IValidable.cs
@@ -3,7 +3,7 @@ namespace AFA.Application.Validators;
 /// <summary>
 /// Interface para marcação de objetos que podem ser validados.
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">Objeto a ser validado</typeparam>
 public interface IValidable<T> where T : new()
 {
 
