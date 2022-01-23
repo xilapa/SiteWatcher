f0a69a4264eeaae2f8c7f5f03893a698fee959cd
dirceu junior <dirceu.sjr@gmail.com>
2022-01-22 22:38:33 -0300

    classe para padronizar retorno da WebAPI

---
+++ src/WebAPI/AFA.WebAPI/DTOs/WebApiResponse.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using AFA.Application.DTOS.Metadata;
+
+namespace AFA.WebAPI.DTOs;
+
+public sealed class WebApiResponse
+{
+    public WebApiResponse()
+    {
+        Messages = new List<string>();
+    }
+
+    public WebApiResponse(object result, params string[] messages)
+    {
+        Messages.AddRange(messages);
+        Result = result;
+    }
+
+    public WebApiResponse(ApplicationResponse appResponse) : this()
+    {
+        if(appResponse.Success)
+        {
+            Messages.Add(appResponse.Message);
+        }
+        else
+        {
+            Messages.AddRange(appResponse.Errors);
+        }
+
+        Result = appResponse.Result;
+    }
+
+    public List<string> Messages { get; set; }
+    public object Result { get; set; }
+}
