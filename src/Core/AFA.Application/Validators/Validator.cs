2e237131288ab7c297b74b2b47427b124b9f91e1
dirceu junior <dirceu.sjr@gmail.com>
2022-01-23 12:57:31 -0300

    tornando Validator thread-safe

--- src/Core/AFA.Application/Validators/Validator.cs
+++ src/Core/AFA.Application/Validators/Validator.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
 using FluentValidation;
@@ -9,13 +9,18 @@ namespace AFA.Application.Validators;
 
 public static class Validator
 {
-    private static Dictionary<Type, object> _validators;
+    static Validator()
+    {
+        // deve ser maior que a quantidade de AbstractValidators no assembly para evitar resize do dicionário
+        var initialCapacity = 32; 
+        var concurrencyLevel = Environment.ProcessorCount;
+        _validators = new ConcurrentDictionary<Type, object>(concurrencyLevel, initialCapacity);
+    }
+
+    private static ConcurrentDictionary<Type, object> _validators;
 
     public static ValidationResult Validate<T>(this IValidable<T> validationTarget) where T : new()
     {
-        if(_validators is null)
-            _validators = new Dictionary<Type, object>();
-
         var targetType = validationTarget.GetType();
         var validatorAbstractBaseType = typeof(AbstractValidator<>);
         var validatorAbstractType = validatorAbstractBaseType.MakeGenericType(targetType);
@@ -30,7 +35,7 @@ public static class Validator
                                     .Single(t => t.IsAssignableTo(validatorAbstractType));
 
             validator = Activator.CreateInstance(validatorType);
-            _validators.Add(validatorAbstractType, validator);
+            _validators.TryAdd(validatorAbstractType, validator);
         }       
 
         var validationResult = (validator as AbstractValidator<T>).Validate((T)validationTarget);
