dd9148d9527698537138283d823e7389d8515757
dirceu junior <dirceu.sjr@gmail.com>
2022-02-05 13:49:10 -0300

    exception response dto

---
+++ src/WebAPI/AFA.WebAPI/DTOs/ExceptionDevResponse.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace AFA.WebAPI.DTOs;
+
+public class ExceptionDevResponse
+{
+    public int Code { get; set; }
+    public string Type { get; set; }
+    public string Message { get; set; }
+    public string TraceId { get; set; }
+    public string StackTrace { get; set; }
+    public InnerExceptionDevResponse InnerException { get; set; }
+
+
+    public static ExceptionDevResponse From(Exception exception, string traceId)
+    {
+        if(exception is null)
+            return null;
+
+        return new ExceptionDevResponse
+                    {
+                        Code = exception.HResult,
+                        Type = exception.GetType().Name,
+                        Message = exception.Message,
+                        TraceId = traceId,
+                        StackTrace = exception.StackTrace,
+                        InnerException = InnerExceptionDevResponse.From(exception.InnerException)
+                    };
+    }
+}
+
+public class InnerExceptionDevResponse
+{
+    public int Code { get; set; }
+    public string Type { get; set; }
+    public string Message { get; set; }
+    public InnerExceptionDevResponse InnerException { get; set; }
+
+    public static InnerExceptionDevResponse From(Exception exception)
+    {
+        if(exception is null)
+            return null;
+
+        return new InnerExceptionDevResponse
+                    {
+                        Code = exception.HResult,
+                        Type = exception.GetType().Name,
+                        Message = exception.Message,
+                        InnerException = From(exception.InnerException)
+                    };
+    }
+}
