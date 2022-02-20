4190cc7f364d6239a5e4d92e6ce3cdc985f4e10f
dirceu junior <dirceu.sjr@gmail.com>
2022-02-20 14:33:42 -0300

    User remade

---
+++ src/Core/SiteWatcher.Domain/Models/User.cs
@@ -0,0 +1,14 @@
+using System;
+using SiteWatcher.Domain.Enums;
+
+namespace SiteWatcher.Domain.Models;
+
+public class User : BaseModel<Guid>
+{
+    public ulong GoogleId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public ELanguage Language { get; set; }
+    public string SecurityStamp { get; set; }
+}
