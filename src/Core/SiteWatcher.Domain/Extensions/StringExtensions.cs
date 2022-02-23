1ed0e84adbc368172ab3bd7f7bc92fb6c8ef02af
dirceu junior <dirceu.sjr@gmail.com>
2022-02-23 12:56:07 -0300

    string extension to get enum equivalent value

--- src/Core/SiteWatcher.Domain/Extensions/StringExtensions.cs
+++ src/Core/SiteWatcher.Domain/Extensions/StringExtensions.cs
@@ -1,5 +1,9 @@
 using System.Security.Cryptography;
 using System.Text;
+using System.Linq;
+using System.Reflection;
+using System.ComponentModel;
+using System;
 
 namespace SiteWatcher.Domain.Extensions;
 
@@ -10,5 +14,31 @@ public static class StringExtensions
         var textBytes = Encoding.ASCII.GetBytes(text);
         var hashedBytes = SHA256.HashData(textBytes);
         return hashedBytes;
+    }
+
+    /// <summary>
+    /// Return the <typeparamref name="EnumT"/> equivalent value of the current string
+    /// based on <typeparamref name="EnumT"/>'s Description Attributes. 
+    /// </summary>
+    /// <typeparam name="EnumT"></typeparam>
+    /// <param name="stringValue"></param>
+    /// <returns></returns>
+    public static EnumT GetEnumValue<EnumT>(this string stringValue) where EnumT : Enum
+    {
+        var enumInstance = Activator.CreateInstance<EnumT>();
+        var enumOptions = typeof(EnumT)
+                            .GetFields(BindingFlags.Public | BindingFlags.Static)
+                            .Select(m => new {
+                                m.Name,
+                                Value = m.GetValue(enumInstance),
+                                DescAtt = m.GetCustomAttributes(typeof(DescriptionAttribute),false).FirstOrDefault()
+                            });
+
+        var option = enumOptions.FirstOrDefault(opts => (opts.DescAtt as DescriptionAttribute).Description == stringValue.ToLower());
+
+        if (option is null)
+            return default;
+
+        return (EnumT) option.Value;
     }
 }
