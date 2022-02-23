590fc98593c31371f34844c25d2f2914ad1bb402
dirceu junior <dirceu.sjr@gmail.com>
2022-02-22 21:21:46 -0300

    http client imported on app module

--- src/UI/SiteWatcher.UI/src/app/app.module.ts
+++ src/UI/SiteWatcher.UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     HomeModule
   ],
