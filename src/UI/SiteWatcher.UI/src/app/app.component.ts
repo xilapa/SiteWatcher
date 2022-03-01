5305fb21eaa781390d98b141a6bda5d9f0f42d42
dirceu junior <dirceu.sjr@gmail.com>
2022-03-01 17:21:28 -0300

    function to check auth redirect added

--- src/UI/SiteWatcher.UI/src/app/app.component.ts
+++ src/UI/SiteWatcher.UI/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { checkAuthRedirect } from './core/auth/auth.utils';
 
 @Component({
   selector: 'sw-root',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SiteWatcher.UI';
+
+  constructor(@Inject(DOCUMENT) doc: Document, router: Router) {
+    checkAuthRedirect(doc.location.href, router);
+  }
 }
