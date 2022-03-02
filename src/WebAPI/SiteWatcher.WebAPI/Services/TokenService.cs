7453b1f85cfa08138a419022344273a3f05783fa
dirceu junior <dirceu.sjr@gmail.com>
2022-03-02 19:20:51 -0300

    token service added

---
+++ src/WebAPI/SiteWatcher.WebAPI/Services/TokenService.cs
@@ -0,0 +1,76 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using SiteWatcher.Domain.Extensions;
+using SiteWatcher.Domain.Models;
+using SiteWatcher.WebAPI.Extensions;
+using SiteWatcher.WebAPI.Settings;
+
+namespace SiteWatcher.WebAPI.Services;
+
+public class TokenService
+{
+    private readonly AppSettings appSettings;
+
+    public TokenService(AppSettings appSettings)
+    {
+        this.appSettings = appSettings;
+    }
+
+    public string GenerateUserToken(ETokenPurpose purpose, User user)
+    {
+        var key = purpose switch
+        {
+            ETokenPurpose.Login => Encoding.ASCII.GetBytes(appSettings.AuthKey),
+            _ => throw new ArgumentException("Value out of range", nameof(purpose)),
+        };
+
+        var claims = new Claim[] 
+        {
+            new ("name", user.Name),
+            new ("email", user.Email),
+            new ("email-confirmed", user.EmailConfirmed.ToString()),
+            new ("language", user.Language.GetDescription())            
+        };
+
+        return GenerateToken(claims, key);
+    }
+
+    public string GenerateRegisterToken(IEnumerable<Claim> tokenClaims, params Claim[] extraClaims)
+    {
+        var key = Encoding.ASCII.GetBytes(appSettings.RegisterKey);
+
+        var defaultClaims = new Claim[] 
+        {
+            tokenClaims.GetClaimValue("email"),
+            tokenClaims.GetClaimValue("name")
+        };
+
+        var claims = defaultClaims.Concat(extraClaims);
+
+        return GenerateToken(claims, key);
+    }
+
+    private static string GenerateToken(IEnumerable<Claim> claims, byte[] key)
+    {
+        var tokenDescriptor = new SecurityTokenDescriptor {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+
+        return tokenString;
+    }
+
+}
+
+public enum ETokenPurpose 
+{
+    Login,
+    ConfirmEmail
+}
--- src/WebAPI/SiteWatcher.WebAPI/Settings/AppSettings.cs
+++ src/WebAPI/SiteWatcher.WebAPI/Settings/AppSettings.cs
@@ -3,4 +3,7 @@ namespace SiteWatcher.WebAPI.Settings;
 public class AppSettings
 {
     public string ConnectionString { get; set;  }
+    public string FrontEndLoginUrl { get; set; }
+    public string RegisterKey { get; set; }
+    public string AuthKey { get; set; }
 }
