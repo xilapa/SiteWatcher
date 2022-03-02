40602b43b0aa54443e7a0927bf5c4aa5a37377ac
dirceu junior <dirceu.sjr@gmail.com>
2022-03-02 19:17:23 -0300

    changed route to home module

--- src/UI/SiteWatcher.UI/src/app/app-routing.module.ts
+++ src/UI/SiteWatcher.UI/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LogInComponent } from './home/log-in/log-in.component';
+
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LogInComponent}
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
