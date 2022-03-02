9b39fab642d7531dfc35e9724e1f4d0d9643569f
dirceu junior <dirceu.sjr@gmail.com>
2022-03-02 19:25:32 -0300

    authentication constant created

---
+++ src/WebAPI/SiteWatcher.WebAPI/Constants/WebApiErrors.cs
@@ -0,0 +1,6 @@
+namespace SiteWatcher.WebAPI.Constants;
+
+public static class WebApiErrors
+{
+    public static readonly string GOOGLE_AUTH_ERROR = "An error has ocurred during Google Authorization";
+}
