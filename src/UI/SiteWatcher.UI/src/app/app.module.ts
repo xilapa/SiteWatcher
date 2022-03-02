4f956f65522b3b94e478cd44863bb323e1923185
dirceu junior <dirceu.sjr@gmail.com>
2022-03-02 19:18:01 -0300

    prime ng toast module added

--- src/UI/SiteWatcher.UI/src/app/app.module.ts
+++ src/UI/SiteWatcher.UI/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+
 
 
 @NgModule({
@@ -14,10 +18,12 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     HttpClientModule,
+    HomeModule,
     AppRoutingModule,
-    HomeModule
-  ],
-  providers: [],
+    ToastModule,
+    BrowserAnimationsModule
+],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
