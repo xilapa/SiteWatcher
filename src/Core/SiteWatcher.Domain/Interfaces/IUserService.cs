c1b7d50b82205c4c041d89f674f30e7c8f60a0f0
dirceu junior <dirceu.sjr@gmail.com>
2022-03-03 21:23:17 -0300

    UserService updated

--- src/Core/SiteWatcher.Domain/Interfaces/IUserService.cs
+++ src/Core/SiteWatcher.Domain/Interfaces/IUserService.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Threading.Tasks;
 using SiteWatcher.Domain.Models;
-using SiteWatcher.Domain.Enums;
 
 namespace SiteWatcher.Domain.Interfaces;
 
 public interface IUserService
 {
-    Task<User> CreateUser(string name, string email);
-    Task<ESubscriptionResult> SubscribeUser(Guid userId);
+    User Register(User user);
 }
--- src/Core/SiteWatcher.Domain/Services/UserService.cs
+++ src/Core/SiteWatcher.Domain/Services/UserService.cs
@@ -1,10 +1,5 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using SiteWatcher.Domain.Models;
-using SiteWatcher.Domain.Enums;
-using SiteWatcher.Domain.Extensions;
 using SiteWatcher.Domain.Interfaces;
+using SiteWatcher.Domain.Models;
 
 namespace SiteWatcher.Domain.Services;
 
@@ -16,48 +11,7 @@ public class UserService : IUserService
         this.userRepository = userRepository;
     }
 
-    public async Task<User> CreateUser(string name, string email)
-    {
-        var userToAdd = new User();
-        userToAdd.SecurityStamp = await GenerateUserSecurityStamp(userToAdd);
-        return this.userRepository.Add(userToAdd);
-    }
+    public User Register(User user) =>    
+        userRepository.Add(user);
 
-    public async Task<ESubscriptionResult> SubscribeUser(Guid userId)
-    {
-        var user = await this.userRepository.FindAsync(u => u.Id == userId);
-
-        if (user is null)
-            return ESubscriptionResult.UserDoesNotExist;            
-
-        // if (user.Subscribed)
-        //     return ESubscriptionResult.AlreadySubscribed;
-
-        // user.Subscribed = true;
-
-        return ESubscriptionResult.SubscribedSuccessfully;
-    }
-
-    private async static Task<string> GenerateUserSecurityStamp(User user)
-    {
-        byte[] securityBytes;
-        using (var stream = new MemoryStream())
-        {
-            if(user.Id != new Guid())
-                await stream.WriteAsync(user.Id.ToString().GetHashedBytes());
-            await stream.WriteAsync(user.Name.GetHashedBytes());
-            await stream.WriteAsync(user.Email.GetHashedBytes());
-            await stream.WriteAsync(DateTime.UtcNow.ToString().GetHashedBytes());
-            await stream.WriteAsync(new Random().Next().ToString().GetHashedBytes());
-
-            var _securityBytes = stream.ToArray();
-            var _securityBytesHash = BitConverter.GetBytes(stream.GetHashCode());
-
-            securityBytes = new byte[_securityBytes.Length + _securityBytesHash.Length];
-            _securityBytes.CopyTo(securityBytes, 0);
-            _securityBytesHash.CopyTo(securityBytes, _securityBytes.Length);
-        }
-
-        return Convert.ToBase64String(securityBytes);
-    }
 }
