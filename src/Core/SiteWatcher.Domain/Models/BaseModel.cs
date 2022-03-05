2d6b33c2e451a8c5bbd2c6333899016c8f2104c0
dirceu junior <dirceu.sjr@gmail.com>
2022-03-04 23:44:41 -0300

    Added LastUpdatedAt on base model

--- src/Core/SiteWatcher.Domain/Models/BaseModel.cs
+++ src/Core/SiteWatcher.Domain/Models/BaseModel.cs
@@ -7,4 +7,5 @@ public abstract class BaseModel<IdType>
     public IdType Id { get; set; }
     public bool Active { get; set; }
     public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdatedAt { get; set; }
 }
