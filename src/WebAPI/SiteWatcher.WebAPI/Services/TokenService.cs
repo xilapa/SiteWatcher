2d99430f40d0b6832c611e60782a15610b63fef9
dirceu junior <dirceu.sjr@gmail.com>
2022-03-04 23:47:52 -0300

    refact token service

--- src/WebAPI/SiteWatcher.WebAPI/Services/TokenService.cs
+++ src/WebAPI/SiteWatcher.WebAPI/Services/TokenService.cs
@@ -15,19 +15,10 @@ public class TokenService : ITokenService
 {
     private readonly AppSettings appSettings;
 
-    public TokenService(AppSettings appSettings)
-    {
-        this.appSettings = appSettings;
-    }
+    public TokenService(AppSettings appSettings) =>  this.appSettings = appSettings;
 
-    public string GenerateUserToken(ETokenPurpose purpose, User user)
+    public string GenerateUserToken(ETokenPurpose tokenPurpose, User user)
     {
-        var key = purpose switch
-        {
-            ETokenPurpose.Login => Encoding.ASCII.GetBytes(appSettings.AuthKey),
-            _ => throw new ArgumentException("Value out of range", nameof(purpose)),
-        };
-
         var claims = new Claim[] 
         {
             new (AuthenticationDefaults.ClaimTypes.Name, user.Name),
@@ -36,30 +27,35 @@ public class TokenService : ITokenService
             new (AuthenticationDefaults.ClaimTypes.Language, ((int)user.Language).ToString())            
         };
 
-        return GenerateToken(claims, key);
+        return GenerateToken(claims, tokenPurpose);
     }
 
     public string GenerateRegisterToken(IEnumerable<Claim> tokenClaims, params Claim[] extraClaims)
-    {
-        var key = Encoding.ASCII.GetBytes(appSettings.RegisterKey);
-
+    {        
         var defaultClaims = new Claim[] 
         {
-            tokenClaims.GetClaimValue("email"),
-            tokenClaims.GetClaimValue("name")
+            tokenClaims.GetClaimValue(AuthenticationDefaults.ClaimTypes.Name),
+            tokenClaims.GetClaimValue(AuthenticationDefaults.ClaimTypes.Email)
         };
 
         var claims = defaultClaims.Concat(extraClaims);
 
-        return GenerateToken(claims, key);
+        return GenerateToken(claims, ETokenPurpose.Register);   
     }
 
-    private static string GenerateToken(IEnumerable<Claim> claims, byte[] key)
+    private string GenerateToken(IEnumerable<Claim> claims, ETokenPurpose tokenPurpose)
     {
+        var key = tokenPurpose switch
+        {
+            ETokenPurpose.Register => appSettings.RegisterKey,
+            ETokenPurpose.Login => appSettings.AuthKey,
+            _ => throw new ArgumentException("Value out of range", nameof(tokenPurpose)),
+        };
+
         var tokenDescriptor = new SecurityTokenDescriptor {
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)), SecurityAlgorithms.HmacSha256Signature)
         };
 
         var tokenHandler = new JwtSecurityTokenHandler();
