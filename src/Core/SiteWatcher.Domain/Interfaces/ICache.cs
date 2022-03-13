566a0b2a34f7c8e7a5816366ee576753293f75c1
dirceu junior <dirceu.sjr@gmail.com>
2022-03-13 14:18:16 -0300

    redis cache added

---
+++ src/Core/SiteWatcher.Domain/Interfaces/ICache.cs
@@ -0,0 +1,26 @@
+namespace SiteWatcher.Domain.Interfaces;
+
+public interface ICache
+{
+    /// <summary>
+    /// Save a <see cref="string"/> to cache
+    /// </summary>
+    Task SaveStringAsync(string key, string value, TimeSpan expiration);
+    Task SaveBytesAsync(string key, byte[] value, TimeSpan expiration);
+
+    /// <summary>
+    /// Get a <see cref="string"/> and remove it from cache
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    Task<string> GetAndRemoveStringAsync(string key);
+
+    /// <summary>
+    /// Get a <see cref="byte"/> array and remove it from cache
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    Task<byte[]> GetAndRemoveBytesAsync(string key);
+    Task<string> GetStringAsync(string key);
+    Task<byte[]> GetBytesAsync(string key);
+}
