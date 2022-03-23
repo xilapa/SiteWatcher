7f8eed64bf9155bfa04c618e11e3cca7fe28316a
dirceu junior <dirceu.sjr@gmail.com>
2022-03-22 23:13:17 -0300

    set different token for registration

--- src/UI/SiteWatcher.UI/src/app/core/auth/auth.service.ts
+++ src/UI/SiteWatcher.UI/src/app/core/auth/auth.service.ts
@@ -3,7 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { AuthenticationResult, ApiResponse } from '../interfaces';
+import { AuthenticationResult, ApiResponse, UserRegister } from '../interfaces';
+import { TokenService } from '../token/token.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +15,44 @@ export class AuthService {
  
   private readonly baseRoute = "google-auth";
 
-  constructor(@Inject(DOCUMENT) private readonly document: Document,
-    private readonly httpClient: HttpClient)
+  constructor(
+    @Inject(DOCUMENT) private readonly document: Document,
+    private readonly httpClient: HttpClient,
+    private readonly tokenService: TokenService,
+    private readonly cookieService: CookieService,
+    private readonly router : Router
+    )
   { }
 
-  public googleLogin(): void{
-    this.document.location.href = `${environment.baseApiUrl}/${this.baseRoute}/login`;
+  public googleLogin(returnUrl: string): void{
+    this.document.location.href = `${environment.baseApiUrl}/${this.baseRoute}/login?returnUrl=${returnUrl}`;
   }
 
-  public googleRegister(): void{
-    this.document.location.href = `${environment.baseApiUrl}/${this.baseRoute}/register`;
+  public googleRegister(returnUrl: string): void{
+    this.document.location.href = `${environment.baseApiUrl}/${this.baseRoute}/register?returnUrl=${returnUrl}`;
   }
 
   public authenticate(state: string, code: string, scope: string): Observable<ApiResponse<AuthenticationResult>> {
     return this.httpClient.post<ApiResponse<AuthenticationResult>>(
       `${environment.baseApiUrl}/${this.baseRoute}/authenticate`,
       { state, code, scope }, {responseType: 'json'})
+  }
+
+  public register(registerData : UserRegister): Observable<ApiResponse<string>> {
+    return this.httpClient.post<ApiResponse<string>>(
+      `${environment.baseApiUrl}/user/register`, registerData, {responseType: 'json', headers: {'authorization': `Bearer ${this.tokenService.getRegisterToken()}`}})
+  }
+
+  public redirecLoggedUser() : void {
+    var cookie = this.cookieService.get('returnUrl');
+
+    if(cookie) {
+      this.cookieService.delete('returnUrl');
+      this.router.navigateByUrl(cookie);
+    } 
+    else {
+      this.router.navigate(['dash']);
+    }        
   }
 
 }
--- src/UI/SiteWatcher.UI/src/app/home/auth/auth.component.ts
+++ src/UI/SiteWatcher.UI/src/app/home/auth/auth.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth/auth.service';
-import { EAuthTask, ELanguage } from 'src/app/core/enums';
-import { ApiResponse, UserRegister } from 'src/app/core/interfaces';
+import { EAuthTask } from 'src/app/core/enums';
+import { ApiResponse } from 'src/app/core/interfaces';
 import { Data } from 'src/app/core/shared-data/shared-data';
 import { MessageService } from 'primeng/api';
-import jwt_decode from "jwt-decode";
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
     selector: 'sw-auth',
@@ -16,7 +16,8 @@ export class AuthComponent implements OnInit {
 
     constructor(private readonly authService: AuthService,
         private readonly router: Router,
-        private readonly messageService: MessageService) { }
+        private readonly messageService: MessageService,
+        private readonly userService: UserService) { }
 
     ngOnInit(): void {
         const url = Data.GetAndRemove('authURL') as URL;
@@ -32,19 +33,19 @@ export class AuthComponent implements OnInit {
         this.authService.authenticate(state, code, scope)
             .subscribe({
                 next: (response) => {
-                    if(response.Result.Task == EAuthTask.Login)
-                        console.log("redirect para dashboard")
-                    
-                    if(response.Result.Task == EAuthTask.Register) {
-                        const userRegister = jwt_decode(response.Result.Token) as UserRegister;
-                        userRegister.language = parseInt(userRegister.language as any) as ELanguage;
-                        Data.Share("register-data", userRegister);
+
+                    if(response.Result.Task == EAuthTask.Register){
+                        this.userService.setUserRegisterData(response.Result.Token);
                         this.router.navigateByUrl('/home/register');
+                    }
+
+                    if(response.Result.Task == EAuthTask.Login){
+                        this.userService.setToken(response.Result.Token);
+                        this.authService.redirecLoggedUser();
                     }
 
                 },
                 error: (errorResponse) => {
-                    console.log((errorResponse.error as ApiResponse<null>).Messages)
                     this.messageService.add(
                         {
                             severity: 'error',
@@ -58,5 +59,4 @@ export class AuthComponent implements OnInit {
                 }
             });
     }
-
 }
