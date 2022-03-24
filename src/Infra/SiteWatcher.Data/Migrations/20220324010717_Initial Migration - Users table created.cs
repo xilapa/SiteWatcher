5aca3ffc3b1fc7ff473275eab00062140b0c5cfb
dirceu junior <dirceu.sjr@gmail.com>
2022-03-23 22:09:17 -0300

    initial migration added

---
+++ src/Infra/SiteWatcher.Data/Migrations/20220324010717_Initial Migration - Users table created.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiteWatcher.Data.Migrations
+{
+    public partial class InitialMigrationUserstablecreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "siteWatcher_webApi");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "siteWatcher_webApi",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    GoogleId = table.Column<string>(type: "varchar(30)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(320)", nullable: false),
+                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    Language = table.Column<short>(type: "smallint", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Unique_User_GoogleId",
+                schema: "siteWatcher_webApi",
+                table: "Users",
+                column: "GoogleId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "siteWatcher_webApi");
+        }
+    }
+}
