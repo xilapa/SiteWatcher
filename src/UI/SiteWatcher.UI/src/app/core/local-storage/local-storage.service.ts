7f8dca356efa18ed93f7fbdd98d24ad966a8f0b2
dirceu junior <dirceu.sjr@gmail.com>
2022-04-15 16:16:47 -0300

    local storage service created

---
+++ src/UI/SiteWatcher.UI/src/app/core/local-storage/local-storage.service.ts
@@ -0,0 +1,23 @@
+import {Inject, Injectable} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+    private storage: Storage;
+
+    constructor(@Inject(DOCUMENT) doc: Document){
+        this.storage = doc.defaultView?.localStorage as Storage;
+    }
+
+    public setItem = (key: string, value: any): void =>
+        this.storage.setItem(key, value);
+
+    public getItem = (key: string): any =>
+        this.storage.getItem(key);
+
+    public removeItem = (key: string): void =>
+        this.storage.removeItem(key);
+}
--- src/UI/SiteWatcher.UI/src/app/core/token/token.service.ts
+++ src/UI/SiteWatcher.UI/src/app/core/token/token.service.ts
@@ -1,23 +1,21 @@
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import {LocalStorageService} from "../local-storage/local-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  private readonly localStorage : Storage;
   private readonly tokenKey = "token";
   private readonly registerTokenKey = "TOKEN";
 
-  constructor(@Inject(DOCUMENT) doc: Document){
-    this.localStorage = doc.defaultView?.localStorage as Storage;
-  }
+  constructor(private readonly localStorage : LocalStorageService)
+  { }
 
   setToken = (token: string) : void =>
     this.localStorage.setItem(this.tokenKey, token);
-  
-  getToken = (): string | null => 
+
+  getToken = (): string | null =>
     this.localStorage.getItem(this.tokenKey);
 
   removeToken = () : void =>
@@ -25,11 +23,10 @@ export class TokenService {
 
   setRegisterToken = (token: string) : void =>
     this.localStorage.setItem(this.registerTokenKey, token);
-  
+
   getRegisterToken = () : string | null =>
     this.localStorage.getItem(this.registerTokenKey);
 
   removeRegisterToken = () : void =>
     this.localStorage.removeItem(this.registerTokenKey);
-  
 }
