5362933457093b7a3ac588e6d34f10e1a76bec9f
dirceu junior <dirceu.sjr@gmail.com>
2022-05-18 22:10:39 -0300

    removing code from user register controller

--- src/Application/Users/Commands/RegisterUser/RegisterUserCommand.cs
+++ src/Application/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -1,4 +1,3 @@
-using SiteWatcher.Application.Users.Commands.RegisterUser;
 using SiteWatcher.Application.Interfaces;
 using AutoMapper;
 using MediatR;
@@ -10,6 +9,7 @@ using SiteWatcher.Domain.Models;
 
 namespace SiteWatcher.Application.Users.Commands.RegisterUser;
 
+//Todo criar interface ICommand que herda de Validable e IRequest
 public class RegisterUserCommand : Validable<RegisterUserCommand>, IRequest<ApplicationResult<string>>
 {
     public RegisterUserCommand() : base(new RegisterUserCommandValidator())
@@ -20,8 +20,15 @@ public class RegisterUserCommand : Validable<RegisterUserCommand>, IRequest<Appl
     public ELanguage Language { get; set; }
     public string? GoogleId { get; set; }
     public string? AuthEmail { get; set; }
+
+    public void GetSessionValues(ISessao sessao)
+    {
+        AuthEmail = sessao.Email;
+        GoogleId = sessao.GoogleId;
+    }
 }
 
+//TODO: fazer o mesmo para o requesthandler, criar uma interface que vai herdar de IRequestHandler
 public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, ApplicationResult<string>>
 {
     private readonly IMapper _mapper;
@@ -29,18 +36,22 @@ public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, A
     private readonly IUserRepository _userRepository;
     private readonly IUnityOfWork _uow;
     private readonly ITokenService _tokenService;
+    private readonly ISessao _sessao;
 
-    public RegisterUserCommandHandler(IMapper mapper, IMediator mediator, IUserRepository userRepository, IUnityOfWork uow, ITokenService tokenService)
+    public RegisterUserCommandHandler(IMapper mapper, IMediator mediator, IUserRepository userRepository,
+        IUnityOfWork uow, ITokenService tokenService, ISessao sessao)
     {
         _mapper = mapper;
         _mediator = mediator;
         _userRepository = userRepository;
         _uow = uow;
         _tokenService = tokenService;
+        _sessao = sessao;
     }
 
     public async Task<ApplicationResult<string>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
+        request.GetSessionValues(_sessao);
         var user = _mapper.Map<User>(request);
         var appResult = new ApplicationResult<string>();
 
@@ -51,10 +62,12 @@ public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, A
         }
         catch(UniqueViolationException ex)
         {
+            // renomear para with error, ou adicionar outro método
             return appResult.AddError(ex.Message);
         }
 
         var token = _tokenService.GenerateLoginToken(user);
+        await _tokenService.InvalidateToken(_sessao.AuthTokenPayload!, ETokenPurpose.Register);
 
         if(!user.EmailConfirmed)
             await _mediator.Publish(_mapper.Map<UserRegisteredNotification>(user), cancellationToken);
--- src/WebAPI/Controllers/UserController.cs
+++ src/WebAPI/Controllers/UserController.cs
@@ -4,9 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using MediatR;
 using SiteWatcher.WebAPI.DTOs.ViewModels;
-using SiteWatcher.WebAPI.Extensions;
-using SiteWatcher.Domain.Enums;
-using SiteWatcher.Application.Constants;
 using SiteWatcher.Application.Users.Commands.RegisterUser;
 
 namespace SiteWatcher.WebAPI.Controllers;
@@ -37,19 +34,7 @@ public class UserController : ControllerBase
     public async Task<IActionResult> Register(RegisterUserCommand registerUserCommand)
     {
         var response = new WebApiResponse<string>();
-        registerUserCommand.AuthEmail = User.Claims.FirstOrDefault(c => c.Type == AuthenticationDefaults.ClaimTypes.Email)?.Value;
-        registerUserCommand.GoogleId = User.Claims.FirstOrDefault(c => c.Type == AuthenticationDefaults.ClaimTypes.GoogleId)?.Value;
-
-        if (registerUserCommand.AuthEmail is null || registerUserCommand.GoogleId is null)
-            return BadRequest(response.AddMessages(ApplicationErrors.INVALID_REGISTER_DATA));
-
         var result = await _mediator.Send(registerUserCommand);
-
-        if (result.Success)
-        {
-            var authTokenPayload = HttpContext.GetAuthTokenPayload();
-            await _tokenService.InvalidateToken(authTokenPayload, ETokenPurpose.Register);
-        }
 
         if (!result.Success)
             return Conflict(response.AddMessages(result.Errors));
