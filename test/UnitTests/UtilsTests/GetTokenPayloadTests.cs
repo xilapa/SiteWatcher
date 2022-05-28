e8b0e7469af23cda8a8e7bbd1108245516031b9a
dirceu junior <dirceu.sjr@gmail.com>
2022-05-27 22:17:49 -0300

    test for get token payload

---
+++ test/UnitTests/UtilsTests/GetTokenPayloadTests.cs
@@ -0,0 +1,24 @@
+﻿using SiteWatcher.Domain.Utils;
+
+namespace UnitTests.UtilsTests;
+
+public class GetTokenPayloadTests
+{
+    private const string Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
+                                 ".eyJpZCI6IjdmkaXJjZXUuc2iOjE2NTM1MjcyODIsImV4cCI6MTY1MzUyNzI5MCwiaWF" +
+                                 ".jX4Srj8_Do7s1B0Ra1tSBE4dQsk8dLUYUY_2Y8ONvXs";
+
+    private const string Payload = "eyJpZCI6IjdmkaXJjZXUuc2iOjE2NTM1MjcyODIsImV4cCI6MTY1MzUyNzI5MCwiaWF";
+
+    [Fact]
+    public void CorretPayloadIsReturned()
+    {
+        // Arrange
+        
+        // Act
+        var payload = Utils.GetTokenPayload(Token);
+        
+        // Assert
+        Assert.Equal(Payload, payload);
+    }
+}
