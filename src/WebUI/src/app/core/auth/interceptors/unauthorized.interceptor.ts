d0be3f9c1526a4771e2f5cef931e223fa96fc37a
dirceu junior <dirceu.sjr@gmail.com>
2022-05-30 18:58:45 -0300

    unauthorized interceptor to work with 403

--- src/WebUI/src/app/core/auth/interceptors/unauthorized.interceptor.ts
+++ src/WebUI/src/app/core/auth/interceptors/unauthorized.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
     HttpRequest,
     HttpHandler,
@@ -6,8 +6,8 @@ import {
     HttpInterceptor,
     HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
-import { Router } from '@angular/router';
+import {Observable, tap} from 'rxjs';
+import {Router} from '@angular/router';
 import {UserService} from "../../user/user.service";
 
 @Injectable()
@@ -16,16 +16,16 @@ export class UnauthorizedInterceptor implements HttpInterceptor {
     constructor(
         private readonly router: Router,
         private readonly userService: UserService
-    ) { }
+    ) {
+    }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         return next.handle(request)
             .pipe(
                 tap({
                     error: err => {
-                        if (err instanceof HttpErrorResponse && err.status == 401){
+                        if (this.userService.isLoggedIn() && err instanceof HttpErrorResponse && (err.status == 401 || err.status == 403)) {
                             this.userService.logout();
-                            this.router.navigateByUrl('/home');
                         }
                     }
                 }));
