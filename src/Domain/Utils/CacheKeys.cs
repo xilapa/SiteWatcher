fee2832460b123b07084e565f869a0d933feab17
dirceu junior <dirceu.sjr@gmail.com>
2022-05-30 18:55:07 -0300

    cache key for invalidated user

---
+++ src/Domain/Utils/CacheKeys.cs
@@ -0,0 +1,8 @@
+﻿using SiteWatcher.Domain.Models.Common;
+
+namespace SiteWatcher.Domain.Utils;
+
+public static class CacheKeys
+{
+    public static string InvalidUser(UserId userId) => $"InvalidUser_{userId}";
+}
