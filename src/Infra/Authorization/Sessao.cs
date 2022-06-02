c54a073b3c7f86857c1d00fe348467ad407bc4e5
dirceu junior <dirceu.sjr@gmail.com>
2022-06-02 19:58:22 -0300

    parsing language at session

--- src/Infra/Authorization/Sessao.cs
+++ src/Infra/Authorization/Sessao.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using SiteWatcher.Application.Interfaces;
+using SiteWatcher.Domain.Enums;
 using SiteWatcher.Domain.Models.Common;
 using SiteWatcher.Infra.Authorization.Constants;
 using SiteWatcher.Infra.Authorization.Extensions;
@@ -19,16 +20,23 @@ public class Sessao : ISessao
 
         Email = Array.Find(claimsEnumerated, c => c.Type == AuthenticationDefaults.ClaimTypes.Email)?.Value;
         GoogleId = Array.Find(claimsEnumerated, c => c.Type == AuthenticationDefaults.ClaimTypes.GoogleId)?.Value;
-        UserName = Array.Find(claimsEnumerated, c => c.Type == AuthenticationDefaults.ClaimTypes.Name)?.Value; 
+        UserName = Array.Find(claimsEnumerated, c => c.Type == AuthenticationDefaults.ClaimTypes.Name)?.Value;
+
+        var hasLang =
+            int.TryParse(Array.Find(claimsEnumerated, c => c.Type == AuthenticationDefaults.ClaimTypes.Language)?.Value,
+                out var lang);
+        Language = hasLang ? (ELanguage) lang : null;
 
         var authenticated = httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
         AuthTokenPayload = authenticated ? httpContextAccessor.HttpContext.GetAuthTokenPayload() : string.Empty;
     }
 
+    // Get the utc date without timezone
     public DateTime Now => new(DateTime.UtcNow.Ticks);
     public UserId? UserId { get; }
     public string? Email { get; }
     public string? GoogleId { get; }
     public string? UserName { get; }
+    public ELanguage? Language { get; }
     public string AuthTokenPayload { get; }
 }
