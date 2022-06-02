a59eff076cac1ada5fef402d800140709d27cd09
dirceu junior <dirceu.sjr@gmail.com>
2022-06-02 19:59:30 -0300

    fix: check missing scopes bench

--- test/Benchmark/Benchs/CheckMissingScopes.cs
+++ test/Benchmark/Benchs/CheckMissingScopes.cs
@@ -1,12 +1,11 @@
 
 using BenchmarkDotNet.Attributes;
-using SiteWatcher.WebAPI.Controllers;
 
 namespace SiteWatcher.Benchmark.Benchs;
 
 [MemoryDiagnoser]
 [MinColumn, MaxColumn]
-public class CheckMissingScopes : GoogleAuthController
+public class CheckMissingScopes
 {
     private readonly string defaultScopes = "apple orange lemon";
     private readonly string scopesToBeChecked = "car apple pepper orange bicycle lemon";
@@ -24,5 +23,33 @@ public class CheckMissingScopes : GoogleAuthController
     public bool CheckScopesWithSpan()
     {
         return IsMissingScope(defaultScopes, scopesToBeChecked);
+    }
+
+    // Same logic implemented at GoogleAuthenticationCommandValidator, but without logging.
+    private bool IsMissingScope(string defaultScopes, string? scopesToBeChecked)
+    {
+        var scopesToBeCheckedSpan = scopesToBeChecked.AsSpan();
+        var scopeSpan = defaultScopes.AsSpan();
+
+        var crrIdx = 0;
+        var sepIdx = 0;
+        var sepDist = scopeSpan.IndexOf(' ');
+
+        while (true)
+        {
+            var scope = scopeSpan.Slice(crrIdx, sepDist);
+
+            if (scopesToBeCheckedSpan.IndexOf(scope) == -1)
+                return true;
+
+            if (sepIdx == -1)
+                break;
+
+            crrIdx = crrIdx + sepDist + 1;
+            sepIdx = scopeSpan[crrIdx..].IndexOf(' ');
+            sepDist = sepIdx == -1 ? scopeSpan.Length - crrIdx : sepIdx;
+        }
+
+        return false;
     }
 }
