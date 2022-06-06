c79a77414bc46a72a8eb78842c7f61885e70193c
dirceu junior <dirceu.sjr@gmail.com>
2022-06-06 19:37:31 -0300

    listen to storage changes to refresh user

--- src/WebUI/src/app/app.component.ts
+++ src/WebUI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {DOCUMENT} from '@angular/common';
-import {Component, Inject} from '@angular/core';
+import {Component, HostListener, Inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthUtils} from './core/auth/auth.utils';
 import {ThemeService} from "./core/theme/theme.service";
 import {LangUtils} from "./core/lang/lang.utils";
 import {TranslocoService} from "@ngneat/transloco";
+import {UserService} from "./core/user/user.service";
 
 @Component({
     selector: 'sw-root',
@@ -19,10 +20,17 @@ export class AppComponent {
         private window: Window,
         private translocoService: TranslocoService,
         router: Router,
-        private readonly themeService: ThemeService
+        private readonly themeService: ThemeService,
+        private readonly userService: UserService
     ) {
         translocoService.setActiveLang(LangUtils.getCurrentBrowserLanguageFileName(window));
         themeService.loadUserTheme();
         AuthUtils.checkAuthAndRedirect(doc.location.href, router);
+    }
+
+    // Listen to storage events to update the user email confirmed status
+    @HostListener('window:storage', ['$event'])
+    onStorageChange(ev: StorageEvent) {
+        this.userService.decodeAndNotify();
     }
 }
