3bc025f4cab17a6bbdfe9c67348430a4fa1b35d0
dirceu junior <dirceu.sjr@gmail.com>
2022-06-23 19:05:59 -0300

    fake http response sequence with queue

--- test/IntegrationTests/Setup/TestServices/FakeHttpDelegateHandler.cs
+++ test/IntegrationTests/Setup/TestServices/FakeHttpDelegateHandler.cs
@@ -1,15 +1,34 @@
-﻿namespace SiteWatcher.IntegrationTests.Setup.TestServices;
+﻿using System.Net;
+using System.Text.Json;
+
+namespace SiteWatcher.IntegrationTests.Setup.TestServices;
 
 public class FakeHttpDelegateHandler : DelegatingHandler
 {
-    private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+    private readonly Queue<FakeHttpResponse> _responses;
 
-    public FakeHttpDelegateHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc)
+    public FakeHttpDelegateHandler(params FakeHttpResponse[] responseSequence)
     {
-        _handlerFunc = handlerFunc;
+        _responses = new Queue<FakeHttpResponse>(responseSequence);
     }
 
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-        CancellationToken cancellationToken) =>
-        _handlerFunc(request, cancellationToken);
+        CancellationToken cancellationToken)
+    {
+        var fakeResponse = _responses.Dequeue();
+        if (fakeResponse.Exception != null)
+            throw fakeResponse.Exception;
+
+        var stringContent = fakeResponse.Response != null ?
+            new StringContent(JsonSerializer.Serialize(fakeResponse.Response)) : new StringContent(string.Empty);
+
+        return Task.FromResult(new HttpResponseMessage(fakeResponse.StatusCode) {Content = stringContent});
+    }
+}
+
+public struct FakeHttpResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public object? Response { get; set; }
+    public HttpRequestException? Exception { get; set; }
 }
