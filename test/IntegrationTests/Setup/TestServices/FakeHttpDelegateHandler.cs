1709ad12a7a7d7bcd692d8d6a71485652afc2bf7
dirceu junior <dirceu.sjr@gmail.com>
2022-06-22 23:02:30 -0300

    fake http delegate handler for tests

---
+++ test/IntegrationTests/Setup/TestServices/FakeHttpDelegateHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace SiteWatcher.IntegrationTests.Setup.TestServices;
+
+public class FakeHttpDelegateHandler : DelegatingHandler
+{
+    private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+
+    public FakeHttpDelegateHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc)
+    {
+        _handlerFunc = handlerFunc;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken) =>
+        _handlerFunc(request, cancellationToken);
+}
