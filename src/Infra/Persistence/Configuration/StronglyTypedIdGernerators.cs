ae2e9d4e5740da9627eb526d6084a54d8816be0d
dirceu junior <dirceu.sjr@gmail.com>
2022-06-25 15:39:16 -0300

    mapping alert to ef core context

---
+++ src/Infra/Persistence/Configuration/StronglyTypedIdGernerators.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using SiteWatcher.Domain.Models.Common;
+
+namespace SiteWatcher.Infra.Configuration;
+
+internal class AlertIdGenerator : ValueGenerator<AlertId>
+{
+    public override bool GeneratesTemporaryValues => true;
+
+    public override AlertId Next(EntityEntry entry) => new (-1);
+}
