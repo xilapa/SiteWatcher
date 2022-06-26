43ea1f6f8308f9b928aadd082d5224fe23fb1398
dirceu junior <dirceu.sjr@gmail.com>
2022-06-26 17:11:31 -0300

    defining relationshio alert > watch mode on ef

--- src/Infra/Persistence/Configuration/AlertMapping.cs
+++ src/Infra/Persistence/Configuration/AlertMapping.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SiteWatcher.Domain.Models.Alerts;
+using SiteWatcher.Domain.Models.Alerts.WatchModes;
 using SiteWatcher.Domain.Models.Common;
 
 namespace SiteWatcher.Infra.Configuration;
@@ -52,5 +53,9 @@ public class AlertMapping : BaseModelMapping<Alert, AlertId>
                         v => new Uri(v))
                     .IsRequired();
             });
+
+        builder.HasOne(a => a.WatchMode)
+            .WithOne()
+            .HasForeignKey(nameof(WatchMode),nameof(WatchMode.AlertId));
     }
 }
