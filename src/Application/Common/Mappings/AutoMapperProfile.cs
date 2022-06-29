b01a80741a5d3ed9d29bb2c72b4b77032d0276a8
dirceu junior <dirceu.sjr@gmail.com>
2022-06-28 21:33:40 -0300

    alert and watch mode auto mapper mappings

--- src/Application/Common/Mappings/AutoMapperProfile.cs
+++ src/Application/Common/Mappings/AutoMapperProfile.cs
@@ -1,9 +1,13 @@
 using AutoMapper;
+using Domain.DTOs.Alert;
 using Domain.Events;
+using SiteWatcher.Application.Alerts.Commands.CreateAlert;
 using SiteWatcher.Application.Interfaces;
 using SiteWatcher.Application.Users.Commands.RegisterUser;
 using SiteWatcher.Application.Users.Commands.UpdateUser;
 using SiteWatcher.Domain.DTOs.User;
+using SiteWatcher.Domain.Models.Alerts;
+using SiteWatcher.Domain.Models.Alerts.WatchModes;
 
 namespace SiteWatcher.Application.Common.Mappings;
 
@@ -16,5 +20,15 @@ public class AutoMapperProfile : Profile
 
         CreateMap<ISession, AccountDeletedEvent>()
             .ForMember(opt => opt.Name, opt => opt.MapFrom(src => src.UserName));
+
+        CreateMap<CreateAlertCommand, CreateAlertInput>();
+        CreateMap<Alert, DetailedAlertView>()
+            .AfterMap<HashIdMapping>();
+
+        CreateMap<WatchMode, DetailedWatchModeView>()
+            .ConstructUsing(src => DetailedWatchModeView.FromModel(src))
+            .AfterMap<HashIdMapping>();
+
+        CreateMap<Site, SiteView>();
     }
 }
---
+++ src/Application/Common/Mappings/HashIdMapping.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Domain.DTOs.Alert;
+using SiteWatcher.Application.Interfaces;
+using SiteWatcher.Domain.Models.Alerts;
+using SiteWatcher.Domain.Models.Alerts.WatchModes;
+
+namespace SiteWatcher.Application.Common.Mappings;
+
+public class HashIdMapping :
+    IMappingAction<Alert, DetailedAlertView>,
+    IMappingAction<WatchMode, DetailedWatchModeView>
+{
+    private readonly IIdHasher _idHasher;
+
+    public HashIdMapping(IIdHasher idHasher)
+    {
+        _idHasher = idHasher;
+    }
+
+    public void Process(WatchMode source, DetailedWatchModeView destination, ResolutionContext context)
+    {
+        destination.Id = _idHasher.HashId(source.Id.Value);
+    }
+
+    public void Process(Alert source, DetailedAlertView destination, ResolutionContext context)
+    {
+        destination.Id = _idHasher.HashId(source.Id.Value);
+    }
+}
