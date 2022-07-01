6f4f1be2c70158ec64b7486b590c6077191a8293
dirceu junior <dirceu.sjr@gmail.com>
2022-06-30 22:02:30 -0300

    uri and watch mode validator for alert create update page

---
+++ src/WebUI/src/app/common/validators/uri.validator.ts
@@ -0,0 +1,17 @@
+﻿import { AbstractControl, ValidationErrors } from "@angular/forms";
+
+export const uriValidator = (control: AbstractControl) :  ValidationErrors | null => {
+    if (control.value && control.value.trim())
+    {
+        let validUrl = true;
+
+        try {
+            new URL(control.value)
+        } catch {
+            validUrl = false;
+        }
+
+        return validUrl ? null : { invalidUrl: true };
+    }
+    return null;
+}
---
+++ src/WebUI/src/app/common/validators/watch-mode-term.validator.ts
@@ -0,0 +1,23 @@
+﻿import {AbstractControl, ValidationErrors} from "@angular/forms";
+import {WatchMode} from "../../alerts/common/watch-mode";
+
+export const watchModeTermValidator = (control: AbstractControl) :  ValidationErrors | null => {
+    const watchMode = (control.parent?.get('watch-mode')?.value as WatchMode);
+    const isTermWatch = watchMode == WatchMode.Term;
+
+    if(!isTermWatch)
+        return null;
+
+    if(!control.value)
+        return { watchModeTermValidator: true };
+
+    if (control.value && control.value.trim().length == 0)
+        return { watchModeTermValidator: true };
+
+    if(control.value.length < 3 || control.value.length > 64)
+        return { watchModeTermValidator: true };
+
+    return null;
+}
+
+
