3a58b48c64e60247d7b5b49c03caf43f42b16ae4
dirceu junior <dirceu.sjr@gmail.com>
2022-07-01 22:18:33 -0300

    button to got to create alert page

--- src/WebUI/src/app/dashboard/dashboard.component.ts
+++ src/WebUI/src/app/dashboard/dashboard.component.ts
@@ -5,33 +5,45 @@ import {TranslocoService} from "@ngneat/transloco";
 import {ELanguage} from "../core/lang/language";
 import {Subscription} from "rxjs";
 import {ThemeService} from "../core/theme/theme.service";
-
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
-  selector: 'sw-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'sw-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 
-  private userSub: Subscription;
+    private userSub: Subscription;
+    private routeEventSub: Subscription;
+    showCreateAlertButton = true;
 
-  constructor(private readonly userService: UserService,
-              private readonly themeService: ThemeService,
-              private readonly translocoService: TranslocoService) {
-  }
+    constructor(private readonly userService: UserService,
+                private readonly themeService: ThemeService,
+                private readonly translocoService: TranslocoService,
+                private readonly router: Router) {
+    }
 
-  ngOnInit(): void {
-    this.themeService.loadUserTheme();
-    this.userSub = this.userService.getUser()
-      .subscribe(user => {
-        this.translocoService.setActiveLang(LangUtils.getLangFileName(user?.language as ELanguage));
-      })
-  }
+    ngOnInit(): void {
 
-  ngOnDestroy(): void {
-    this.userSub?.unsubscribe();
-  }
+        this.routeEventSub = this.router.events.subscribe(event => {
+            if(event instanceof NavigationEnd){
+                const slashIndex = event?.urlAfterRedirects.lastIndexOf('/');
+                const activePage = this.router.url.substring(slashIndex + 1);
+                this.showCreateAlertButton = activePage == 'dash';
+            }
+        })
 
+        this.themeService.loadUserTheme();
+        this.userSub = this.userService.getUser()
+            .subscribe(user => {
+                this.translocoService
+                    .setActiveLang(LangUtils.getLangFileName(user?.language as ELanguage));
+            })
+    }
 
+    ngOnDestroy(): void {
+        this.userSub?.unsubscribe();
+        this.routeEventSub?.unsubscribe();
+    }
 }
