c860b085de32546a85068cd94c109f7900ee93fb
dirceu junior <dirceu.sjr@gmail.com>
2022-07-02 15:25:00 -0300

    keyset paginated query to get user alerts

--- src/Infra/Persistence/DapperRepositories/DapperQueries.cs
+++ src/Infra/Persistence/DapperRepositories/DapperQueries.cs
@@ -18,4 +18,24 @@ public class DapperQueries : IDapperQueries
                 ""siteWatcher_webApi"".""Users"" AS u
             WHERE
                 u.""Id"" = @userId AND u.""Active"" ";
+
+    public virtual string GetSimpleAlertViewListByUserId => @"
+            SELECT 
+                a.""Id"",
+	            a.""Name"",
+	            a.""CreatedAt"",
+                a.""Frequency"",
+                a.""LastVerification"",
+                a.""Site_Name"" SiteName,
+                wm.""WatchMode""
+            FROM 
+            ""siteWatcher_webApi"".""Alerts"" a
+                INNER JOIN ""siteWatcher_webApi"".""WatchModes"" wm 
+                    ON a.""Id"" = wm.""AlertId""
+            WHERE
+                a.""Id"" > @lastAlertId
+                AND a.""UserId"" = @userId
+            ORDER BY 
+                a.""Id""
+            LIMIT @take";
 }
