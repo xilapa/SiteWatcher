b1e175d85e961ccd26e7ef8ccc024c610d785e1f
dirceu junior <dirceu.sjr@gmail.com>
2022-07-04 19:35:29 -0300

    get alerts cache invalidation

---
+++ src/Domain/Events/Alerts/AlertCreatedOrUpdatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using SiteWatcher.Domain.Models.Common;
+
+namespace Domain.Events.Alerts;
+
+public class AlertCreatedOrUpdatedEvent : BaseEvent
+{
+    public AlertCreatedOrUpdatedEvent(UserId userId)
+    {
+        UserId = userId;
+    }
+
+    public UserId UserId { get; set; }
+}
