2c51eb4fd3774417abbe8991f76959131b3716dd
dirceu junior <dirceu.sjr@gmail.com>
2022-07-04 19:32:05 -0300

    Filter attribute to cache results automagically

---
+++ src/Application/Interfaces/ICacheable.cs
@@ -0,0 +1,8 @@
+﻿namespace SiteWatcher.Application.Interfaces;
+
+public interface ICacheable
+{
+    public TimeSpan Expiration { get; }
+    public string HashFieldName { get; }
+    string GetKey(ISession session);
+}
---
+++ src/WebAPI/Filters/Cache/CacheInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace SiteWatcher.WebAPI.Filters.Cache;
+
+public class CacheInfo
+{
+    public string Key { get; set; } = null!;
+    public string HashFieldName { get; set; } = null!;
+    public TimeSpan Expiration { get; set; }
+}
