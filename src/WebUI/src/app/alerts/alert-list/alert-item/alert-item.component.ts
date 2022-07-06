17efeef29ced266b87a6184409d0f6f6055762ef
dirceu junior <dirceu.sjr@gmail.com>
2022-07-06 20:45:07 -0300

    alert item - display date base on locale

--- src/WebUI/src/app/alerts/alert-list/alert-item/alert-item.component.ts
+++ src/WebUI/src/app/alerts/alert-list/alert-item/alert-item.component.ts
@@ -1,30 +1,43 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {DetailedAlertView} from "../../common/alert";
 import {DeviceService} from "../../../core/device/device.service";
-import {Observable} from "rxjs";
 import {AlertUtils} from "../../common/alert-frequency";
 import {WatchModeUtils} from "../../common/e-watch-mode";
+import {TranslocoService} from "@ngneat/transloco";
+import {LangUtils} from "../../../core/lang/lang.utils";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'sw-alert-item',
     templateUrl: './alert-item.component.html',
     styleUrls: ['./alert-item.component.css']
 })
-export class AlertItemComponent implements OnInit {
+export class AlertItemComponent implements OnInit, OnDestroy {
 
     @Input() alert: DetailedAlertView;
     isMobile: boolean;
     frequencyTranslationKey : string;
     watchModeTranslationKey : string;
+    private deviceSub : Subscription;
+    private readonly currentLocale: string;
+    dateStringLocalized: string;
 
-    constructor(private deviceService : DeviceService) {
-
-        deviceService.isMobileScreen().subscribe(mobile => this.isMobile = mobile);
+    constructor(private deviceService : DeviceService,
+                private readonly transloco: TranslocoService,
+                private window : Window) {
+        this.deviceSub = deviceService.isMobileScreen().subscribe(mobile => this.isMobile = mobile);
+        this.currentLocale = LangUtils.getCurrentLocale(window);
     }
 
     ngOnInit(): void {
         this.frequencyTranslationKey = AlertUtils.getFrequencyTranslationKey(this.alert.Frequency);
         this.watchModeTranslationKey = WatchModeUtils.getWatchModeTranslationKey(this.alert.WatchMode.WatchMode);
+        this.dateStringLocalized = this.alert.CreatedAt.toLocaleDateString(this.currentLocale) + ' '
+            + this.alert.CreatedAt.toLocaleTimeString(this.currentLocale);
+    }
+
+    ngOnDestroy(): void {
+        this.deviceSub?.unsubscribe();
     }
 
 }
