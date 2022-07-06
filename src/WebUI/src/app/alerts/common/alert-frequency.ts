fc09d71a2309f91083c26c82c15820d49a249154
dirceu junior <dirceu.sjr@gmail.com>
2022-07-05 22:15:22 -0300

    frequency and watch modes helpers

--- src/WebUI/src/app/alerts/common/alert-frequency.ts
+++ src/WebUI/src/app/alerts/common/alert-frequency.ts
@@ -6,3 +6,20 @@
     TwelveHours = 12,
     TwentyFourHours = 24
 }
+
+export class AlertUtils{
+    public static getFrequencyTranslationKey(frequency: AlertFrequency): string{
+        switch (frequency) {
+            case AlertFrequency.TwoHours:
+                return "alert.frequency.twoHours";
+            case AlertFrequency.FourHours:
+                return "alert.frequency.fourHours";
+            case AlertFrequency.EightHours:
+                return "alert.frequency.eightHours";
+            case AlertFrequency.TwelveHours:
+                return "alert.frequency.twelveHours";
+            case AlertFrequency.TwentyFourHours:
+                return "alert.frequency.twentyFourHours";
+        }
+    }
+}
--- src/WebUI/src/app/alerts/common/e-watch-mode.ts
+++ src/WebUI/src/app/alerts/common/e-watch-mode.ts
@@ -2,3 +2,14 @@
     AnyChanges = 1,
     Term
 }
+
+export class WatchModeUtils{
+    public static getWatchModeTranslationKey(watchMode: EWatchMode): string{
+        switch (watchMode) {
+            case EWatchMode.AnyChanges:
+                return "alert.watchMode.anyChanges";
+            case EWatchMode.Term:
+                return "alert.watchMode.term";
+        }
+    }
+}
