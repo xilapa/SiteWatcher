fb7c8232325cf6bab64bbb9a5ee3d3d4d109f680
Dirceu Junior <dirceu.sjr@gmail.com>
2022-07-16 15:33:39 -0300

    event handler to delete watch mode

---
+++ src/Application/Alerts/EventHandlers/AlertWatchModeChangedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Events.Alerts;
+using MediatR;
+using SiteWatcher.Application.Interfaces;
+
+namespace SiteWatcher.Application.Alerts.EventHandlers;
+
+public class AlertWatchModeChangedEventHandler : INotificationHandler<AlertWatchModeChangedEvent>
+{
+    private readonly IAlertRepository _alertRepository;
+
+    public AlertWatchModeChangedEventHandler(IAlertRepository alertRepository)
+    {
+        _alertRepository = alertRepository;
+    }
+
+    public Task Handle(AlertWatchModeChangedEvent notification, CancellationToken cancellationToken)
+    {
+        _alertRepository.DeleteWatchMode(notification.OldWatchModeId);
+        return Task.CompletedTask;
+    }
+}
