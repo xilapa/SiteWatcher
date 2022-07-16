3b6b09c06d776976d21a9b5d2c559cab4e70ae3b
Dirceu Junior <dirceu.sjr@gmail.com>
2022-07-16 15:33:14 -0300

    repository methods to get alert and delete watchmode

--- src/Application/Interfaces/IAlertRepository.cs
+++ src/Application/Interfaces/IAlertRepository.cs
@@ -1,6 +1,10 @@
 ﻿using SiteWatcher.Domain.Models.Alerts;
+using SiteWatcher.Domain.Models.Common;
 
 namespace SiteWatcher.Application.Interfaces;
 
 public interface IAlertRepository : IRepository<Alert>
-{ }
+{
+    Task<Alert?> GetAlertForUpdate(AlertId alertId, UserId userId);
+    void DeleteWatchMode(WatchModeId watchModeId);
+}
--- src/Infra/Persistence/Repositories/AlertRepository.cs
+++ src/Infra/Persistence/Repositories/AlertRepository.cs
@@ -1,15 +1,56 @@
 ﻿using System.Linq.Expressions;
+using Domain.DTOs.Alert;
+using Microsoft.EntityFrameworkCore;
 using SiteWatcher.Application.Interfaces;
 using SiteWatcher.Domain.Models.Alerts;
+using SiteWatcher.Domain.Models.Alerts.WatchModes;
+using SiteWatcher.Domain.Models.Common;
 
 namespace SiteWatcher.Infra.Repositories;
 
 public class AlertRepository : Repository<Alert>, IAlertRepository
 {
-    public AlertRepository(SiteWatcherContext context) : base(context) { }
+    public AlertRepository(SiteWatcherContext context) : base(context)
+    { }
 
     public override Task<Alert?> GetAsync(Expression<Func<Alert, bool>> predicate, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<Alert?> GetAlertForUpdate(AlertId alertId, UserId userId)
+    {
+        var updateAlertDto = await Context.Alerts
+            .Where(a => a.Id == alertId &&
+                        a.UserId == userId &&
+                        a.Active)
+            .Select(alert => new UpdateAlertDto
+            {
+                Id = alert.Id,
+                UserId = alert.UserId,
+                CreatedAt = alert.CreatedAt,
+                Name = alert.Name,
+                Frequency = alert.Frequency,
+                SiteName = alert.Site.Name,
+                SiteUri = alert.Site.Uri,
+                WatchMode = alert.WatchMode
+            })
+            .AsSplitQuery()
+            .SingleOrDefaultAsync();
+
+        if (updateAlertDto is null)
+            return null!;
+
+        var alert = Alert.GetModelForUpdate(updateAlertDto);
+        // Attach alert and site, because the watch mode is already tracked
+        Context.Attach(alert);
+        return alert;
+    }
+
+    public void DeleteWatchMode(WatchModeId watchModeId)
+    {
+        var watchMode = Context.ChangeTracker.Entries<WatchMode>()
+            .Single(w => w.Entity.Id == watchModeId);
+        watchMode.State = EntityState.Deleted;
     }
 }
