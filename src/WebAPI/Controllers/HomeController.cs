35b12a9d342b0ce84a8077622d28f6c048a1cf42
Dirceu Junior <dirceu.sjr@gmail.com>
2022-07-19 20:31:51 -0300

    route to wake-up the server

--- src/WebAPI/Controllers/HomeController.cs
+++ src/WebAPI/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using SiteWatcher.Application.Interfaces;
 using SiteWatcher.Infra;
 using SiteWatcher.WebAPI.DTOs.ViewModels;
 using SiteWatcher.WebAPI.Filters;
-using SiteWatcher.WebAPI.Settings;
 
 namespace SiteWatcher.WebAPI.Controllers;
 
@@ -11,25 +9,15 @@ namespace SiteWatcher.WebAPI.Controllers;
 [Route("")]
 public class HomeController : ControllerBase
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IAppSettings _settings;
     private readonly DatabaseMigrator _migrator;
 
-    public HomeController(ILogger<HomeController> logger, IAppSettings settings, DatabaseMigrator migrator)
+    public HomeController(DatabaseMigrator migrator)
     {
-        _logger = logger;
-        _settings = settings;
         _migrator = migrator;
     }
 
     [HttpGet]
-    public IActionResult Get()
-    {
-        // TODO: Arrumar ou remover, pois como está não funciona
-        var originIp = Request.HttpContext.Connection.RemoteIpAddress;
-        _logger.LogInformation("Someone from {Ip} tried to access the main api url at {Date}", originIp, DateTime.UtcNow);
-        return Redirect(_settings.FrontEndUrl);
-    }
+    public IActionResult Get() => Ok();
 
     [ApiKey]
     [HttpPost("migrate")]
--- src/WebUI/src/app/app.component.ts
+++ src/WebUI/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import {DOCUMENT} from '@angular/common';
-import {Component, HostListener, Inject} from '@angular/core';
+import {Component, HostListener, Inject, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthUtils} from './core/auth/auth.utils';
 import {ThemeService} from "./core/theme/theme.service";
 import {LangUtils} from "./core/lang/lang.utils";
 import {TranslocoService} from "@ngneat/transloco";
 import {UserService} from "./core/user/user.service";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {first} from "rxjs";
 
 @Component({
     selector: 'sw-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     title = 'SiteWatcher.UI';
 
     constructor(
@@ -21,7 +24,8 @@ export class AppComponent {
         private translocoService: TranslocoService,
         router: Router,
         private readonly themeService: ThemeService,
-        private readonly userService: UserService
+        private readonly userService: UserService,
+        private readonly httpClient: HttpClient
     ) {
         translocoService.setActiveLang(LangUtils.getCurrentBrowserLanguageFileName(window));
         themeService.loadUserTheme();
@@ -33,4 +37,10 @@ export class AppComponent {
     onStorageChange(ev: StorageEvent) {
         this.userService.decodeAndNotify();
     }
+
+    ngOnInit(): void {
+        // call the server on init to wake it up
+        this.httpClient.get(environment.baseApiUrl).pipe(first()).subscribe();
+    }
+
 }
