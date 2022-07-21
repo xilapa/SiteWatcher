df57ef234c67e68862f9022f66d7a9e0ac4a3073
Dirceu Junior <dirceu.sjr@gmail.com>
2022-07-20 21:05:04 -0300

    unit test for ToLowerCaseWithoutDiacritics

---
+++ test/UnitTests/ExtensionsTests/StringExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using SiteWatcher.Domain.Extensions;
+
+namespace UnitTests.ExtensionsTests;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineData("Macarrão", "macarrao")]
+    [InlineData("Wikipédia", "wikipedia")]
+    public void StringIsLowerCaseAndDiacriticsAreRemoved(string input, string expected)
+    {
+        // Act
+        var result = input.ToLowerCaseWithoutDiacritics();
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
