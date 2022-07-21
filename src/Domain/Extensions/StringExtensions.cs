0caf05cf189202aad8a28a88ea9b3d9629db7043
Dirceu Junior <dirceu.sjr@gmail.com>
2022-07-20 21:00:53 -0300

    string extension method to lower case and remove diacritics

--- src/Domain/Extensions/StringExtensions.cs
+++ src/Domain/Extensions/StringExtensions.cs
@@ -1,5 +1,8 @@
+using System.Buffers;
 using System.Reflection;
 using System.ComponentModel;
+using System.Globalization;
+using System.Text;
 
 namespace SiteWatcher.Domain.Extensions;
 
@@ -28,5 +31,39 @@ public static class StringExtensions
             return default;
 
         return (EnumT) option.Value!;
+    }
+
+    public static string ToLowerCaseWithoutDiacritics(this string text)
+    {
+        if (text.Length == 0)
+            return text;
+
+        // Max length based on https://dotnet.github.io/dotNext/features/io/rental.html
+        Span<char> res = text.Length > 128 ? ArrayPool<char>.Shared.Rent(text.Length) : stackalloc char[text.Length];
+
+        // the normalization to FormD splits accented letters in letters+accents
+        var normalizedString = text.Normalize(NormalizationForm.FormD).AsSpan();
+
+        var resCounter = 0;
+        var normalizedStringCounter = 0;
+        foreach (var rune in normalizedString.EnumerateRunes())
+        {
+            var runeCategory = Rune.GetUnicodeCategory(rune);
+            if (runeCategory == UnicodeCategory.NonSpacingMark)
+            {
+                // move to the next character on normalized string
+                normalizedStringCounter++;
+                continue;
+            }
+
+            res[resCounter] = runeCategory == UnicodeCategory.UppercaseLetter
+                ? char.ToLower(normalizedString[normalizedStringCounter])
+                : normalizedString[normalizedStringCounter];
+
+            normalizedStringCounter++;
+            resCounter++;
+        }
+
+        return res.ToString();
     }
 }
