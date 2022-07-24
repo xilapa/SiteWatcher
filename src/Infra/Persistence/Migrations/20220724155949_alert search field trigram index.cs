d46301c3835274ec4bd6089e21c690ed2873178a
Dirceu Junior <dirceu.sjr@gmail.com>
2022-07-24 14:57:13 -0300

    trigram index on alert search field

---
+++ src/Infra/Persistence/Migrations/20220724155949_alert search field trigram index.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    public partial class alertsearchfieldtrigramindex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                -- https://www.postgresql.org/docs/current/pgtrgm.html
+                CREATE EXTENSION pg_trgm WITH SCHEMA ""siteWatcher_webApi"";
+                -- More about indexes for like queries https://niallburkley.com/blog/index-columns-for-like-in-postgres/
+                CREATE INDEX IX_Alert_SearchField_TRIGRAM ON ""siteWatcher_webApi"".""Alerts"" USING gin(""SearchField"" gin_trgm_ops);
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP INDEX IX_Alert_SearchField_TRIGRAM;
+                DROP EXTENSION pg_trgm;
+            ");
+        }
+    }
+}
