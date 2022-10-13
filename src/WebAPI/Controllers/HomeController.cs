fd8f6680284afa576090e474b6b11b5bc3b21cf5
Dirceu Junior <dirceu.sjr@gmail.com>
2022-10-12 22:31:40 -0300

    allow anonymous on health check route

--- src/WebAPI/Controllers/HomeController.cs
+++ src/WebAPI/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SiteWatcher.Infra;
 using SiteWatcher.WebAPI.Filters;
@@ -16,6 +17,7 @@ public class HomeController : ControllerBase
     }
 
     [HttpGet]
+    [AllowAnonymous]
     public IActionResult Get() => Ok();
 
     [ApiKey]
