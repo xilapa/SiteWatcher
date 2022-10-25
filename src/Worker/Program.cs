a8b4cded1d22fe291b5cc5b3790f921ac2d18afe
xilapa <dirceu.sjr@gmail.com>
2022-10-25 19:28:22 -0300

    worker project created

---
+++ src/Worker/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureDefaults(args)
+    .ConfigureHostConfiguration(configBuilder =>
+        configBuilder
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddEnvironmentVariables("DOTNET_")
+            .AddCommandLine(args)
+    )
+    .ConfigureAppConfiguration((hostContext, configBuilder) => 
+        configBuilder
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{hostContext.HostingEnvironment}.json", true, true)
+            .AddEnvironmentVariables("DOTNET_")
+            .AddCommandLine(args)
+    )
+    // .ConfigureServices()
+    .Build();
+
+await host.RunAsync();
