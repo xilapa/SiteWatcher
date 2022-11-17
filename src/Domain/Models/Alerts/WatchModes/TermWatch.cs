a968b1228f1fed54161108bfdcc74b017c111b1f
xilapa <junior.dirceu@pm.me>
2022-11-17 20:12:14 -0300

    term watch site verification logic

--- src/Domain/Models/Alerts/WatchModes/TermWatch.cs
+++ src/Domain/Models/Alerts/WatchModes/TermWatch.cs
@@ -1,4 +1,6 @@
-﻿using Domain.DTOs.Alerts;
+﻿using System.Text.RegularExpressions;
+using Domain.DTOs.Alerts;
+using SiteWatcher.Domain.Utils;
 
 namespace SiteWatcher.Domain.Models.Alerts.WatchModes;
 
@@ -21,14 +23,53 @@ public class TermWatch : WatchMode
         Term = updateAlertInput.Term.NewValue!;
     }
 
-    public override Task<bool> VerifySite(Stream html)
+    public override async Task<bool> VerifySite(Stream html)
     {
-        // TODO:
-        throw new NotImplementedException();
+        var htmlExtractedText = await HtmlUtils.ExtractText(html);
+
+        // The max length of Term is 64, the max length of Ocurrence.Context is 512.
+        // So, there are left 448 characters to get, 224 for each side.
+        var pattern = $"([^.]{{0,224}}{Term}[^.]{{0,224}}\\.?)";
+        var matches = Regex.Matches(htmlExtractedText, pattern, RegexOptions.IgnoreCase);
+
+        // Save the ocurrences for future comparison
+        if (!FirstWatchDone)
+        {
+            _occurrences.AddRange(matches.Select(_ => new TermOccurrence(_.Value.Trim())));
+            FirstWatchDone = true;
+            return false;
+        }
+
+        var matchesArray = matches.Select(_ => _.Value.Trim()).ToArray();
+        var ocurrencesArray = _occurrences.Select(_ => _.Context).ToArray();
+
+        // If it has differences, save it
+        bool SaveDifferenceAndReturn(bool different)
+        {
+            if(!different) return false;
+            _occurrences.Clear();
+            _occurrences.AddRange(matchesArray!.Select(v => new TermOccurrence(v)));
+            return true;
+        }
+
+        // First compare the length of current matches and the saved occurrences
+        var differentLength = matchesArray.Length != ocurrencesArray.Length;
+        if(differentLength)
+            return SaveDifferenceAndReturn(differentLength);
+
+        // If the length are equals, compare if the matches are in the occurrences
+        var differentOccurrence = false;
+        foreach(var m in matchesArray)
+        {
+            differentOccurrence = !ocurrencesArray.Contains(m);
+            if(differentOccurrence) break;
+        }
+
+        return SaveDifferenceAndReturn(differentOccurrence);
     }
 
     public string Term { get; private set; }
-    private readonly IEnumerable<TermOccurrence> _occurrences;
+    private readonly List<TermOccurrence> _occurrences;
     public IReadOnlyCollection<TermOccurrence> Occurrences => _occurrences.ToArray();
 }
 
