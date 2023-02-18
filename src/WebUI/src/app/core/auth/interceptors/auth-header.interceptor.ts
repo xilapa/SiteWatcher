96c2c3bcc93b171259ccd38390e167b4e0423539
xilapa <junior.dirceu@pm.me>
2023-02-17 21:14:40 -0300

    setting all request to send credentials

--- src/WebUI/src/app/core/auth/interceptors/auth-header.interceptor.ts
+++ src/WebUI/src/app/core/auth/interceptors/auth-header.interceptor.ts
@@ -1,22 +1,20 @@
-import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+  HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {TokenService} from "../../token/token.service";
+import { TokenService } from "../../token/token.service";
 
 @Injectable()
 export class AuthHeaderInterceptor implements HttpInterceptor {
 
-  constructor(private tokenService: TokenService) {}
+  constructor(private tokenService: TokenService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.tokenService.getToken();
-    if(token)
-      request = request.clone({ setHeaders: { 'authorization': `Bearer ${token}`} })
+    if (token)
+      // request = request.clone({ setHeaders: { 'authorization': `Bearer ${token}`} })
+      request = request.clone({ withCredentials: true })
 
     return next.handle(request);
   }
