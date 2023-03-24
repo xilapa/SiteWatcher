1347b7d28dab0476f4281f5bc58917cae41c55c2
xilapa <junior.dirceu@pm.me>
2023-03-24 18:18:55 -0300

    tests for auth start route

---
+++ test/IntegrationTests/AuthenticationTests/AuthStartTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using FluentAssertions;
+using IntegrationTests.Setup;
+using SiteWatcher.Infra.Authorization.Constants;
+
+namespace IntegrationTests.AuthenticationTests;
+
+public sealed class AuthStartTest : BaseTest, IClassFixture<BaseTestFixture>
+{
+    public AuthStartTest(BaseTestFixture fixture) : base(fixture)
+    { }
+
+    [Theory]
+    [InlineData(AuthenticationDefaults.Schemes.Google, true)]
+    [InlineData("invalid", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    public async Task StartAuthWorks(string? schema, bool redirects)
+    {
+        // act
+        var res = await GetAsync($"auth/start/{schema}");
+
+        // assert
+        if (redirects)
+        {
+            res.HttpResponse!.StatusCode.Should().Be(HttpStatusCode.Redirect);
+            res.HttpResponse.Headers.Location.Should().NotBeNull();
+            return;
+        }
+
+        res.HttpResponse!.StatusCode
+            .Should()
+            .BeOneOf(HttpStatusCode.BadRequest, HttpStatusCode.Unauthorized);
+    }
+}
