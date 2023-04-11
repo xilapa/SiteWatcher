f5700fe055d7952c50ae011bcce62dc274cfc625
xilapa <junior.dirceu@pm.me>
2023-04-10 21:27:13 -0300

    repository method to get user by Id

--- src/Domain/Common/Repositories/IDapperQueries.cs
+++ src/Domain/Common/Repositories/IDapperQueries.cs
@@ -2,6 +2,7 @@
 public interface IDapperQueries
 {
     string GetUserByGoogleId { get; }
+    string GetUserById { get; }
     string DeleteActiveUserById { get; }
     string GetSimpleAlertViewListByUserId { get; }
     string GetAlertDetails { get; }
--- src/Infra/Persistence/DapperRepositories/UserDapperRepository.cs
+++ src/Infra/Persistence/DapperRepositories/UserDapperRepository.cs
@@ -1,5 +1,3 @@
-using System.Threading;
-using System.Threading.Tasks;
 using Dapper;
 using SiteWatcher.Common.Repositories;
 using SiteWatcher.Domain.Common.ValueObjects;
@@ -25,6 +23,13 @@ public class UserDapperRepository : IUserDapperRepository
             cancellationToken: cancellationToken);
         return await _dapperContext.UsingConnectionAsync(conn =>
             conn.QuerySingleOrDefaultAsync<UserViewModel>(commandDefinition));
+    }
+
+    public Task<UserViewModel?> GetUserByIdAsync(UserId id, CancellationToken ct)
+    {
+        var cmmdDef = new CommandDefinition(_dapperQueries.GetUserById, new { id }, cancellationToken: ct);
+        return _dapperContext.UsingConnectionAsync(conn =>
+            conn.QuerySingleOrDefaultAsync<UserViewModel?>(cmmdDef));
     }
 
     public async Task<bool> DeleteActiveUserAsync(UserId userId, CancellationToken cancellationToken)
