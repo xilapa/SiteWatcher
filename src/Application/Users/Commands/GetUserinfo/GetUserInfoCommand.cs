8dafedc55e4ff446b42d4d2010174b4ba76e2aee
xilapa <junior.dirceu@pm.me>
2023-04-10 21:27:26 -0300

    get user info command

---
+++ src/Application/Users/Commands/GetUserinfo/GetUserInfoCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SiteWatcher.Domain.Common.ValueObjects;
+using SiteWatcher.Domain.Users.DTOs;
+using SiteWatcher.Domain.Users.Repositories;
+
+namespace SiteWatcher.Application.Users.Commands.GetUserinfo;
+
+public sealed class GetUserInfoCommand : IRequest<UserViewModel?>
+{
+    public UserId UserId { get; set; }
+}
+
+public sealed class GetUserInfoCommandHandler : IRequestHandler<GetUserInfoCommand, UserViewModel?>
+{
+    private readonly IUserDapperRepository _userRepo;
+
+    public GetUserInfoCommandHandler(IUserDapperRepository userRepo)
+    {
+        _userRepo = userRepo;
+    }
+
+    public Task<UserViewModel?> Handle(GetUserInfoCommand request, CancellationToken ct)
+    {
+        if (UserId.Empty.Equals(request.UserId))
+            return Task.FromResult<UserViewModel?>(null);
+
+        return _userRepo.GetUserByIdAsync(request.UserId, ct);
+    }
+}
