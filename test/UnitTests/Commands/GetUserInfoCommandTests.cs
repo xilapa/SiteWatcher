aa3fc6b0f49e2a599913fc2ca2d2af7b3008e539
xilapa <junior.dirceu@pm.me>
2023-04-12 19:41:57 -0300

    tests

---
+++ test/UnitTests/Commands/GetUserInfoCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Moq;
+using SiteWatcher.Application.Users.Commands.GetUserinfo;
+using SiteWatcher.Domain.Authentication;
+using SiteWatcher.Domain.Common.ValueObjects;
+
+namespace UnitTests.Commands;
+
+public class GetUserInfoCommandTests
+{
+    [Fact]
+    public async Task GetUserInfoReturnsNullWhenIdIsEmpty()
+    {
+        // Arrange
+        var sessionMock = new Mock<ISession>();
+        sessionMock.Setup(s => s.UserId).Returns(UserId.Empty);
+
+        var cmmd = new GetUserInfoCommand();
+        var handler = new GetUserInfoCommandHandler(null!, sessionMock.Object);
+
+        // Act
+        var res = await handler.Handle(cmmd, CancellationToken.None);
+
+        // Assert
+        res.Should().BeNull();
+    }
+}
--- test/UnitTests/Commands/UpdateUserCommandTests.cs
+++ test/UnitTests/Commands/UpdateUserCommandTests.cs
@@ -6,9 +6,9 @@ using SiteWatcher.Application.Common.Constants;
 using SiteWatcher.Application.Users.Commands.UpdateUser;
 using SiteWatcher.Common.Repositories;
 using SiteWatcher.Domain.Authentication;
-using SiteWatcher.Domain.Authentication.Services;
 using SiteWatcher.Domain.Users;
 using SiteWatcher.Domain.Users.Repositories;
+using SiteWatcher.IntegrationTests.Setup.TestServices;
 
 namespace UnitTests.Commands;
 
@@ -23,10 +23,9 @@ public sealed class UpdateUserCommandTests
             .Setup(u => u.GetAsync(It.IsAny<Expression<Func<User, bool>>>(), It.IsAny<CancellationToken>()))
             .Returns(Task.FromResult<User?>(default));
         var uow = new Mock<IUnitOfWork>().Object;
-        var authService = new Mock<IAuthService>().Object;
         var session = new Mock<ISession>().Object;
 
-        var commandHandler = new UpdateUserCommandHandler(userRepositoryMock.Object, uow, authService, session);
+        var commandHandler = new UpdateUserCommandHandler(userRepositoryMock.Object, uow, session, new FakeCache());
 
         // Act
         var result = await commandHandler.Handle(new UpdateUserCommand(), CancellationToken.None) as ErrorResult;
