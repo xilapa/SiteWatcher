842a52f1051d48d2787bd6877b45cd1894cd1bf0
xilapa <junior.dirceu@pm.me>
2023-06-14 07:18:25 -0300

    tet containers fix

--- test/IntegrationTests/Setup/TestSettings.cs
+++ test/IntegrationTests/Setup/TestSettings.cs
@@ -6,4 +6,9 @@ public sealed class TestSettings
     /// Testcontainers docker host environment variable.
     /// </summary>
     public string DOCKER_HOST { get; set; }
+
+    /// <summary>
+    /// Testcontainers, file path to the Docker daemon socket that is used by Ryuk (resource reaper).
+    /// </summary>
+    public string TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE { get; set; }
 }
