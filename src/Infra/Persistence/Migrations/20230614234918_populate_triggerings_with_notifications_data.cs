4aec4ee803439855691f3e02f48c0e9fc127f6fa
xilapa <junior.dirceu@pm.me>
2023-06-15 07:05:07 -0300

    populate triggering from notifications

---
+++ src/Infra/Persistence/Migrations/20230614234918_populate_triggerings_with_notifications_data.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class populate_triggerings_with_notifications_data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            INSERT INTO ""sw"".""Triggerings""(""Date"", ""AlertId"")
+            SELECT ""CreatedAt"", ""AlertId"" FROM ""sw"".""Notifications"";");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM ""sw"".""Triggerings"";");
+        }
+    }
+}
