a5f76064e1eaa9dd48417d59904b87282fde9bef
xilapa <junior.dirceu@pm.me>
2023-06-20 07:29:28 -0300

    populate new email fields

---
+++ src/Infra/Persistence/Migrations/20230620102735_populate_email_createdat_lastupdatedat.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class populate_email_createdat_lastupdatedat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+UPDATE ""sw"".""Emails"" e
+            SET 
+            ""CreatedAt"" = sub.""CreatedAt"",
+            ""LastUpdatedAt"" = sub.""CreatedAt""
+            FROM (
+                SELECT n.""EmailId"", n.""CreatedAt""
+            FROM ""sw"".""Notifications"" n
+                WHERE ""EmailId"" IS NOT NULL
+                ) sub
+                WHERE
+            e.""Id"" = sub.""EmailId""
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
