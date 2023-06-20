ba63554db0bac8c988baaef85e45e427d6875729
xilapa <junior.dirceu@pm.me>
2023-06-19 21:27:54 -0300

    populate NotificationAlerts

---
+++ src/Infra/Persistence/Migrations/20230618191451_populate_notificationalerts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class populate_notificationalerts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+-- SET UserId on Notification
+UPDATE ""sw"".""Notifications"" n
+SET ""UserId"" = e.""UserId""
+FROM ""sw"".""Emails"" e
+    WHERE n.""EmailId"" = e.""Id"";
+
+-- Populate Notification Alerts
+INSERT INTO ""sw"".""NotificationAlerts""(""NotificationId"",""AlertId"",""TriggeringDate"")
+SELECT ""Id"",""AlertId"", ""CreatedAt"" FROM ""sw"".""Notifications"";");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
