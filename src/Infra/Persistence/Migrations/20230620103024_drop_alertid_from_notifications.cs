6a30993b2b7ba6cfe5b2a3ac2dc9c0dcc2ac8a88
xilapa <junior.dirceu@pm.me>
2023-06-20 07:31:29 -0300

    drop AlertId from Notifications

---
+++ src/Infra/Persistence/Migrations/20230620103024_drop_alertid_from_notifications.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class drop_alertid_from_notifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"ALTER TABLE ""sw"".""Notifications"" DROP COLUMN ""AlertId"";");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
