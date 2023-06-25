a7897b11ff0b4bb2bc71ea4483e18a04e285b16f
xilapa <junior.dirceu@pm.me>
2023-06-25 17:43:30 -0300

    moving the frequencies logic to domain

---
+++ src/Domain/DomainServices/AlertFrequencies.cs
@@ -0,0 +1,22 @@
+﻿using SiteWatcher.Domain.Alerts.Enums;
+
+namespace SiteWatcher.Domain.DomainServices;
+
+public static class AlertFrequencies
+{
+    public static List<Frequencies> GetCurrentFrequencies(DateTime currentTime)
+    {
+        var alertFrequenciesToWatch = new List<Frequencies>();
+
+        var currentHour = currentTime.Hour;
+
+        // If the rest of current hour/ frequency is zero, this alerts of this frequency needs to be watched
+        foreach (var frequency in Enum.GetValues<Frequencies>())
+        {
+            if (currentHour % (int)frequency == 0)
+                alertFrequenciesToWatch.Add(frequency);
+        }
+
+        return alertFrequenciesToWatch;
+    }
+}
--- src/Worker/Jobs/ExecuteAlertsPeriodically.cs
+++ src/Worker/Jobs/ExecuteAlertsPeriodically.cs
@@ -2,7 +2,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SiteWatcher.Application.Alerts.Commands.ExecuteAlerts;
 using SiteWatcher.Application.Interfaces;
-using SiteWatcher.Domain.Alerts.Enums;
+using SiteWatcher.Domain.Authentication;
+using SiteWatcher.Domain.DomainServices;
 
 namespace SiteWatcher.Worker.Jobs;
 
@@ -24,28 +25,12 @@ public sealed class ExecuteAlertsPeriodically : BackgroundService
             {
                 await using var scope = _scopeFactory.CreateAsyncScope();
                 var handler = scope.ServiceProvider.GetRequiredService<ExecuteAlertsCommandHandler>();
+                var session = scope.ServiceProvider.GetRequiredService<ISession>();
 
-                var frequencies = GetAlertFrequenciesForCurrentHour();
+                var frequencies = AlertFrequencies.GetCurrentFrequencies(session.Now);
                 await handler.Handle(new ExecuteAlertsCommand(frequencies), stoppingToken);
 
                 await _timer.WaitForNextTickAsync(stoppingToken);
             }
         }, stoppingToken);
-
-    private static List<Frequencies> GetAlertFrequenciesForCurrentHour()
-    {
-        var alertFrequenciesToWatch = new List<Frequencies>();
-
-        // TODO: Wrapper for time, to make tests possible
-        var currentHour = DateTime.Now.Hour;
-
-        // If the rest of current hour/ frequency is zero, this alerts of this frequency needs to be watched
-        foreach (var frequency in Enum.GetValues<Frequencies>())
-        {
-            if (currentHour % (int)frequency == 0)
-                alertFrequenciesToWatch.Add(frequency);
-        }
-
-        return alertFrequenciesToWatch;
-    }
 }
