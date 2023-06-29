c109c9cb0d13386bfed6fa9ce4091f3bfa1fb7a5
xilapa <junior.dirceu@pm.me>
2023-06-29 20:13:25 -0300

    adjust on testcontainers settings

--- test/IntegrationTests/Setup/TestSettings.cs
+++ test/IntegrationTests/Setup/TestSettings.cs
@@ -6,9 +6,4 @@ public sealed class TestSettings
     /// Testcontainers docker host environment variable.
     /// </summary>
     public string DOCKER_HOST { get; set; }
-
-    /// <summary>
-    /// Testcontainers, file path to the Docker daemon socket that is used by Ryuk (resource reaper).
-    /// </summary>
-    public string TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE { get; set; }
 }
