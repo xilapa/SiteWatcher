72561434ba23b06dacea85d6ed3ccd7e3e948c83
xilapa <junior.dirceu@pm.me>
2023-07-03 09:21:34 -0300

    separating save changes and commiting transactions

--- src/Infra/Messaging/Publisher.cs
+++ src/Infra/Messaging/Publisher.cs
@@ -5,18 +5,15 @@ namespace SiteWatcher.Infra.Messaging;
 
 public sealed class Publisher : IPublisher
 {
-    private readonly SiteWatcherContext _ctx;
     private readonly ICapPublisher _capPublisher;
 
-    public Publisher(SiteWatcherContext ctx, ICapPublisher capPublisher)
+    public Publisher(ICapPublisher capPublisher)
     {
-        _ctx = ctx;
         _capPublisher = capPublisher;
     }
 
     public async Task PublishAsync(string routingKey, object message, Dictionary<string, string>? headers, CancellationToken ct)
     {
-        await using var trx = await _ctx.Database.BeginTransactionAsync(_capPublisher, autoCommit: false, cancellationToken: ct);
         headers ??= new Dictionary<string, string>(1);
         headers.Add("content-type","application/json");
         await _capPublisher.PublishAsync(routingKey, message, headers!, ct);
