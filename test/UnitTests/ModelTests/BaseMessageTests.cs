450b8630741fc8ec1d34cb1b671e5cd40676c9c8
xilapa <junior.dirceu@pm.me>
2023-07-04 22:10:34 -0300

    test to avoid message serialization errors

---
+++ test/UnitTests/ModelTests/BaseMessageTests.cs
@@ -0,0 +1,20 @@
+﻿using SiteWatcher.Domain.Common.Messages;
+
+namespace UnitTests.ModelTests;
+
+public sealed class BaseMessageTests
+{
+    [Fact]
+    public void MessageImplementationShouldHaveEmptyPublicConstructor()
+    {
+        var messages = typeof(BaseMessage).Assembly.GetTypes()
+            .Where(t => !t.IsAbstract && t.IsSubclassOf(typeof(BaseMessage)))
+            .ToArray();
+
+        foreach (var message in messages)
+        {
+            var hasTheCtor = message.GetConstructors().Any(c => c.GetParameters().Length == 0);
+            Assert.True(hasTheCtor, $"{message.Name} should have an empty public constructor");
+        }
+    }
+}
