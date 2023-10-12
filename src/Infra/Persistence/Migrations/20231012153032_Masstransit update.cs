cb493dd2ad872118241c8b0c8d343fdde1df1aee
xilapa <junior.dirceu@pm.me>
2023-10-12 12:32:23 -0300

    Masstransit migration

---
+++ src/Infra/Persistence/Migrations/20231012153032_Masstransit update.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Masstransitupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MessageType",
+                schema: "sw",
+                table: "OutboxMessage",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MessageType",
+                schema: "sw",
+                table: "OutboxMessage");
+        }
+    }
+}
