7e6dcd3423689f76b29da62eb12e3d061558b306
xilapa <junior.dirceu@pm.me>
2023-11-09 21:36:20 -0300

    Added CreationDate to IdempotentConsumer

--- src/Domain/Common/ValueObjects/IdempotentConsumer.cs
+++ src/Domain/Common/ValueObjects/IdempotentConsumer.cs
@@ -2,6 +2,7 @@ namespace SiteWatcher.Domain.Common.ValueObjects;
 
 public class IdempotentConsumer
 {
+    public DateTime DateCreated { get; set; }
     public string MessageId { get; set; } = null!;
     public string Consumer { get; set; } = null!;
 }
